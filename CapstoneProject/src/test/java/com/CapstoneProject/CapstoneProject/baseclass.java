2b0e5557820874315d7ef97f6f401306498ae611
BasanagoudPatil <basanagoudp329@gmail.com>
2024-06-16 20:53:07 +0530

    Added extent report

--- CapstoneProject/src/test/java/com/CapstoneProject/CapstoneProject/baseclass.java
+++ CapstoneProject/src/test/java/com/CapstoneProject/CapstoneProject/baseclass.java
@@ -5,27 +5,47 @@ import java.time.Duration;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.Status;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
 
 public class baseclass {
-WebDriver driver;
-	
-	@BeforeClass
-	public void driver()
-	{
-		System.setProperty("webdriver.chrome.driver", "D:/Wipro/Project/driver/chromedriver-win64/chromedriver.exe");
-		driver=new ChromeDriver();
-		driver.manage().window().maximize();
-		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));	
+    WebDriver driver;
+    ExtentReports extent;
+    ExtentTest test;
 
-		//driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-	}
-	
-	@AfterClass
-	public void close()
-	{
-		driver.quit();
-	}
+    @BeforeClass
+    public void setup() {
+        ExtentSparkReporter spark=new ExtentSparkReporter("./extent-reports/extent-report.html");
+        extent=new ExtentReports();
+        extent.attachReporter(spark);
 
+        System.setProperty("webdriver.chrome.driver", "D:/Wipro/Project/driver/chromedriver-win64/chromedriver.exe");
+        driver=new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));  
+    }
+    
+    @BeforeMethod
+    public void beforeMethod() 
+    {
+        test=extent.createTest("Test Case: "+this.getClass().getName());
+    }
 
+    @AfterMethod
+    public void afterMethod() 
+    {
+        extent.flush();
+    }
+
+    @AfterClass
+    public void close() {
+        driver.quit();
+        extent.flush();
+    }
 }
--- CapstoneProject/src/test/java/com/CapstoneProject/CapstoneProject/search.java
+++ CapstoneProject/src/test/java/com/CapstoneProject/CapstoneProject/search.java
@@ -5,138 +5,163 @@ import java.io.IOException;
 import java.util.List;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.io.FileHandler;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
-import org.testng.Assert;
+import com.aventstack.extentreports.Status;
 
-public class search extends baseclass{
-	@Test(priority=0)
+public class search extends baseclass {
+
+    @Test(priority=0)
     public void findsearchbox() throws InterruptedException, IOException {
-        driver.get("https://jpetstore.aspectran.com/");
-        // Your test code here
-        WebElement searchbox=driver.findElement(By.xpath("//*[@id=\"SearchContent\"]/form/div/input"));
-        File screenshot=((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
-        FileHandler.copy(screenshot,new File("./screenshots/searchbox.png"));
-
-        Thread.sleep(2000);
-    }
-	
-	@Test(priority=1)
-	public void sendkeys() throws InterruptedException, IOException
-	{
-	 String item="Dog";
-     driver.get("https://jpetstore.aspectran.com/");
-     WebElement searchbox=driver.findElement(By.xpath("//*[@id=\"SearchContent\"]/form/div/input"));
-     searchbox.sendKeys(item);
-     File screenshot=((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
-     FileHandler.copy(screenshot,new File("./screenshots/sendkeys.png"));
-
-     Thread.sleep(2000);
-	}
-	@Test(priority=2)
-	public void search() throws InterruptedException, IOException
-	{
-		driver.get("https://jpetstore.aspectran.com/");
-	     WebElement searchbox=driver.findElement(By.xpath("//*[@id=\"SearchContent\"]/form/div/input"));
-	     searchbox.sendKeys("Fish");
-	     WebElement search=driver.findElement(By.cssSelector("#SearchContent > form > div > div > button"));
-	     search.click();
-	     WebElement tableBody=driver.findElement(By.xpath("//*[@id=\"Catalog\"]/table/tbody"));
-
-	        List<WebElement> rows=tableBody.findElements(By.tagName("tr"));
-
-	        boolean hasDataRows=false;
-	        for (WebElement row : rows) {
-	            List<WebElement> cells=row.findElements(By.tagName("td"));
-	            if (cells.size()>0) {
-	                hasDataRows=true;
-	                break;
-	            }
-	        }
-	        File screenshot=((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
-	        FileHandler.copy(screenshot,new File("./screenshots/search.png"));
-
-	        // Assert that no data rows are present
-	        Assert.assertTrue(hasDataRows, "results not found");
-	        
-	}
-	@Test(priority=3)
-	public void validsearch() throws IOException {
-		String item="goldfish";
-		WebElement searchbox=driver.findElement(By.xpath("//*[@id=\"SearchContent\"]/form/div/input"));
-	     searchbox.sendKeys(item);
-	     searchbox.submit();
-        // Verify the search results
-        WebElement result = driver.findElement(By.xpath("//*[@id=\"Catalog\"]/table/tbody/tr[2]/td[2]"));
-        File screenshot=((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
-        FileHandler.copy(screenshot,new File("./screenshots/validsearch.png"));
-
-        Assert.assertTrue(result.getText().toLowerCase().contains(item), "Search result failed to get valid result");
-        
-
-    }
-	
-	@Test(priority=4)
-	public void emptysearch() throws InterruptedException, IOException
-	{
-		WebElement searchbox = driver.findElement(By.xpath("//*[@id=\"SearchContent\"]/form/div/input"));
-        searchbox.clear();
-        searchbox.submit();
-        Thread.sleep(2000);
-
-        WebElement tableBody=driver.findElement(By.xpath("//*[@id=\"Catalog\"]/table/tbody"));
-
-        List<WebElement> rows=tableBody.findElements(By.tagName("tr"));
-
-        boolean hasDataRows=false;
-        for (WebElement row : rows) {
-            List<WebElement> cells=row.findElements(By.tagName("td"));
-            if (cells.size()>0) {
-                hasDataRows=true;
-                break;
-            }
+        test = extent.createTest("findsearchbox");
+        try {
+            driver.get("https://jpetstore.aspectran.com/");
+            WebElement searchbox = driver.findElement(By.xpath("//*[@id=\"SearchContent\"]/form/div/input"));
+            File screenshot = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+            FileHandler.copy(screenshot, new File("./screenshots/searchbox.png"));
+            test.log(Status.PASS, "Search box found and screenshot taken");
+        } catch (Exception e) {
+            test.log(Status.FAIL, "Test case findsearchbox failed");
+            test.fail(e);
         }
-        File screenshot=((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
-        FileHandler.copy(screenshot,new File("./screenshots/emptysearch.png"));
+        Thread.sleep(2000);
+    }
 
-        // Assert that no data rows are present
-        Assert.assertFalse(hasDataRows, "result found");
-	}
-	
-	@Test(priority=5)
-	public void invalidsearch() throws IOException
-	{
-		String item="Invalid search";
-		WebElement searchbox=driver.findElement(By.xpath("//*[@id=\"SearchContent\"]/form/div/input"));
-	     searchbox.sendKeys(item);
-	     searchbox.submit();
-        // Verify the search results
-	     WebElement tableBody=driver.findElement(By.xpath("//*[@id=\"Catalog\"]/table/tbody"));
+    @Test(priority=1)
+    public void sendkeys() throws InterruptedException, IOException {
+        test = extent.createTest("sendkeys");
+        try {
+            String item = "Dog";
+            driver.get("https://jpetstore.aspectran.com/");
+            WebElement searchbox = driver.findElement(By.xpath("//*[@id=\"SearchContent\"]/form/div/input"));
+            searchbox.sendKeys(item);
+            File screenshot = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+            FileHandler.copy(screenshot, new File("./screenshots/sendkeys.png"));
+            test.log(Status.PASS, "Sent keys to search box and screenshot taken");
+        } catch (Exception e) {
+            test.log(Status.FAIL, "Test case sendkeys failed");
+            test.fail(e);
+        }
+        Thread.sleep(2000);
+    }
 
-	        List<WebElement> rows=tableBody.findElements(By.tagName("tr"));
+    @Test(priority=2)
+    public void search() throws InterruptedException, IOException {
+        test = extent.createTest("search");
+        try {
+            driver.get("https://jpetstore.aspectran.com/");
+            WebElement searchbox=driver.findElement(By.xpath("//*[@id=\"SearchContent\"]/form/div/input"));
+            searchbox.sendKeys("Fish");
+            WebElement search=driver.findElement(By.cssSelector("#SearchContent > form > div > div > button"));
+            search.click();
+            WebElement tableBody=driver.findElement(By.xpath("//*[@id=\"Catalog\"]/table/tbody"));
+            List<WebElement> rows=tableBody.findElements(By.tagName("tr"));
 
-	        boolean hasDataRows=false;
-	        for (WebElement row : rows) {
-	            List<WebElement> cells=row.findElements(By.tagName("td"));
-	            if (cells.size()>0) {
-	                hasDataRows=true;
-	                break;
-	            }
-	        }
-	        File screenshot=((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
-	        FileHandler.copy(screenshot,new File("./screenshots/invalidsearch.png"));
+            boolean hasDataRows=false;
+            for (WebElement row : rows) {
+                List<WebElement> cells=row.findElements(By.tagName("td"));
+                if (cells.size()>0) {
+                    hasDataRows = true;
+                    break;
+                }
+            }
+            File screenshot=((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+            FileHandler.copy(screenshot,new File("./screenshots/search.png"));
 
+            Assert.assertTrue(hasDataRows, "results not found");
+            test.log(Status.PASS, "Search functionality working, screenshot taken");
+        } catch (Exception e) {
+            test.log(Status.FAIL, "Test case search failed");
+            test.fail(e);
+        }
+        Thread.sleep(2000);
+    }
 
-	        // Assert that no data rows are present
-	        Assert.assertFalse(hasDataRows, "Result Found");
-	}
-         
+    @Test(priority=3)
+    public void validsearch() throws IOException {
+        test = extent.createTest("validsearch");
+        try {
+            String item="goldfish";
+            WebElement searchbox = driver.findElement(By.xpath("//*[@id=\"SearchContent\"]/form/div/input"));
+            searchbox.sendKeys(item);
+            searchbox.submit();
+
+            WebElement result=driver.findElement(By.xpath("//*[@id=\"Catalog\"]/table/tbody/tr[2]/td[2]"));
+            File screenshot=((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+            FileHandler.copy(screenshot,new File("./screenshots/validsearch.png"));
+
+            Assert.assertTrue(result.getText().toLowerCase().contains(item), "Search result failed to get valid result");
+            test.log(Status.PASS, "Valid search, screenshot taken");
+        } catch (Exception e) {
+            test.log(Status.FAIL, "Test case validsearch failed");
+            test.fail(e);
+        }
+    }
+
+    @Test(priority=4)
+    public void emptysearch() throws InterruptedException, IOException {
+        test=extent.createTest("emptysearch");
+        try {
+            WebElement searchbox = driver.findElement(By.xpath("//*[@id=\"SearchContent\"]/form/div/input"));
+            searchbox.clear();
+            searchbox.submit();
+            Thread.sleep(2000);
+
+            WebElement tableBody=driver.findElement(By.xpath("//*[@id=\"Catalog\"]/table/tbody"));
+            List<WebElement> rows=tableBody.findElements(By.tagName("tr"));
+
+            boolean hasDataRows = false;
+            for (WebElement row : rows) {
+                List<WebElement> cells=row.findElements(By.tagName("td"));
+                if (cells.size()>0) {
+                    hasDataRows=true;
+                    break;
+                }
+            }
+            File screenshot=((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+            FileHandler.copy(screenshot, new File("./screenshots/emptysearch.png"));
+
+            Assert.assertFalse(hasDataRows, "result found");
+            test.log(Status.PASS, "Empty search, screenshot taken");
+        } catch (Exception e) {
+            test.log(Status.FAIL, "Test case emptysearch failed");
+            test.fail(e);
+        }
+    }
+
+    @Test(priority=5)
+    public void invalidsearch() throws IOException {
+        test = extent.createTest("invalidsearch");
+        try {
+            String item="Invalid search";
+            WebElement searchbox=driver.findElement(By.xpath("//*[@id=\"SearchContent\"]/form/div/input"));
+            searchbox.sendKeys(item);
+            searchbox.submit();
+
+            WebElement tableBody=driver.findElement(By.xpath("//*[@id=\"Catalog\"]/table/tbody"));
+            List<WebElement> rows=tableBody.findElements(By.tagName("tr"));
+
+            boolean hasDataRows=false;
+            for (WebElement row:rows) {
+                List<WebElement> cells=row.findElements(By.tagName("td"));
+                if (cells.size()>0) {
+                    hasDataRows=true;
+                    break;
+                }
+            }
+            File screenshot=((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+            FileHandler.copy(screenshot, new File("./screenshots/invalidsearch.png"));
+
+            Assert.assertFalse(hasDataRows,"Result Found");
+            test.log(Status.PASS, "Invalid search, screenshot taken");
+        } catch (Exception e) {
+            test.log(Status.FAIL, "Test case invalidsearch failed");
+            test.fail(e);
+        }
+    }
 }
-	
-	
-
